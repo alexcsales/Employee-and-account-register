4b3a706d63fffbad6a70271efaa1fb564d941509
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-02-18 12:40:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  agencia;
-  #saldo = 0;
-
-  sacar(valor) {
-    if (this.#saldo >= valor)
-    this.#saldo -= valor;
-    console.log("valor sacado: " + valor,"||saldo atual: " + this.#saldo);
-
-  }
-
-  depositar(valor) {
-    if (valor > 0)
-    this.#saldo += valor;
-    console.log("valor depositado: " + valor,"||saldo atual: " + this.#saldo);
-  }
-}
-
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(200);
-
 contaCorrenteRicardo.sacar(50);
 
 const cliente2 = new Cliente();
