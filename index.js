d14816918d64e978e8203c49d23e24aca7191b46
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-02-19 11:17:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(200);
-contaCorrenteRicardo.sacar(50);
+const conta1 = new ContaCorrente();
+conta1.agencia = 1001;
+conta1.cliente = cliente1;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
+
+const conta2 = new ContaCorrente();
+conta2.agencia = 1002;
+conta2.cliente = cliente2;
+
+conta1.depositar(500);
+conta1.transferir(200, conta2)
+
+console.log(conta1);
+console.log(conta2);
+conta1.Saldo();
+conta2.Saldo();
