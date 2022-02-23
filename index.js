4db2af5b006121cb2b6257f2c5648a86d0c61053
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-02-23 13:09:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionarios/Gerente.js"
+import { Diretor } from "./Funcionarios/Diretor.js"
+import { SistemaAutentificacao } from "./SistemaAutentificacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const conta1 = new ContaCorrente(1001, cliente1);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha(123456)
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha(654321)
 
-const cliente2 = new Cliente("Alice", 88822233309);
-const conta2 = new ContaCorrente(102, cliente2);
+const diretorEstaLogado = SistemaAutentificacao.login(diretor, 123456)
+const gerenteEstaLogado = SistemaAutentificacao.login(gerente, 654321)
 
-console.log(cliente1);
-conta1.depositar(500);
-conta1.transferir(200,conta2);
-
-console.log(cliente2);
-conta2.sacar(100);
-conta2.transferir(50,conta1);
-
-console.log(conta1.saldo)
-console.log(conta2.saldo)
+console.log(diretorEstaLogado, gerenteEstaLogado)
