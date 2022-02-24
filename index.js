ec7ece5f9181af03eb686a6d5afbd974652431ff
Alex Corrêa Sales <88143360+alexcsales@users.noreply.github.com>
2022-02-24 13:13:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import { Cliente } from "./Cliente.js";
-import { Gerente } from "./Funcionarios/Gerente.js"
-import { Diretor } from "./Funcionarios/Diretor.js"
-import { SistemaAutentificacao } from "./SistemaAutentificacao.js";
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const diretor = new Diretor("Rodrigo", 10000, 12345678900);
-diretor.cadastrarSenha(123456)
-const gerente = new Gerente("Ricardo", 5000, 12378945601);
-gerente.cadastrarSenha(654321)
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const diretorEstaLogado = SistemaAutentificacao.login(diretor, 123456)
-const gerenteEstaLogado = SistemaAutentificacao.login(gerente, 654321)
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-console.log(diretorEstaLogado, gerenteEstaLogado)
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
